1a1e965d81d0ed6c36d55bd3536bdf2bf3cfd7b6
JacobLErickson <jacob.erickson.bravo@gmail.com>
2019-07-25 16:34:04 -0500

    FizzBuuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
 
 public class ControlFlowExercises {
-    public static void main(String[] args) {
-//
-//  c.
-        for (int i = 5; i <= 15; i++) {
-            System.out.println(i);
-        }
 
-        for(long x = 2; x < 1000000; x *= x) {
-            System.out.println(x);
+//    2.
+    static void fizzBuzz(int num) {
+        for (int i = 1; i <= num; i++) {
+            if (i % 15 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (i%5==0) {
+                System.out.println("buzz");
+            } else if (i%3==0) {
+                System.out.println("fizz");
+            } else {
+                System.out.println(i);
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        fizzBuzz(33);
+
     }
 }
