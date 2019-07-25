adf8d3d71410cc1c77b5d7b8d07f00541d887cab
JacobLErickson <jacob.erickson.bravo@gmail.com>
2019-07-25 16:58:21 -0500

    started table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
 
 public class ControlFlowExercises {
-
-//    2.
-    static void fizzBuzz(int num) {
-        for (int i = 1; i <= num; i++) {
-            if (i % 15 == 0) {
-                System.out.println("fizzbuzz");
-            } else if (i%5==0) {
-                System.out.println("buzz");
-            } else if (i%3==0) {
-                System.out.println("fizz");
-            } else {
-                System.out.println(i);
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        fizzBuzz(33);
 
+//  3.
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What number would you like to go up to? \n");
+
+        long x = scanner.nextInt();
+
+        System.out.println("Here is your table!\n");
+        System.out.println("number | squared | cubed ");
+        System.out.println("------ | ------- | ----- ");
+        for(long i = 1; i <= x; i++) {
+            long i2 = i *= i;
+            long i3 = i *= i *= i;
+            System.out.printf("%d     | %d      | %d    \n", i, i2, i3);
+        }
     }
 }
